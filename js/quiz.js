e52f4f231652836582d48fb64de09910005ef3fb
davidallanturner <David.Turner@usa.win.int.kn>
2021-10-04 16:54:42 -0600

    Syntax fix

--- js/quiz.js
+++ js/quiz.js
@@ -30,7 +30,7 @@ let quiz = {
     ,  {response: 'I served 7 years at Fort Bliss, where I received numerous commendations, including several from the Joint Chiefs of Staff.'
     ,answer: positiveAnsw
     }
-    ,   {response: 'Fun fact: Nope, but I have been an Elder of my church for a long time now. When the pastor is out, I\'ve stepped up. If we didn\'t have a pastor, I filled that role. (Not that anyone asked, but I\'m Lutheran, and I do have schooling for the pastoral profession. If I take that profession farther, I\'ll be doing freelancing on the side.)'
+    ,   {response: 'Fun fact: I have not, but I have been an Elder of my church for a long time now. When the pastor is out, I\'ve stepped up. If we didn\'t have a pastor, I filled that role. (Not that anyone asked, but I\'m Lutheran, and I do have schooling for the pastoral profession. If I take that profession farther, I\'ll be doing freelancing on the side.)'
     ,answer: negativeAnsw
     }
     ,   {response: 'My lucky number is 17!'
