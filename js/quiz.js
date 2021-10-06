288612d0f7c3fa26478fecc9a50a9be99674288d
davidallanturner <David.Turner@usa.win.int.kn>
2021-10-06 17:14:14 -0600

    Fixed number guess, it was an issue of type as prompt is only a txt not number. Also added 7th question. Have not worked that into the system yet.

--- js/quiz.js
+++ js/quiz.js
@@ -17,6 +17,7 @@ let quiz = {
     'Did I serve in the Army?',
     "Do you think I've ever been a pastor before?",
     'Guess my favorite number.',
+    '"Don\'t use very sad, use morose. Language was invented for one reason, boys - to woo women - and, in that endeavor, laziness will not do. It also won\'t do in your essays." Give me a synonym for love in the phrase "I love you." (One word only!)',
   ],
   answers: [
     {
@@ -44,7 +45,9 @@ let quiz = {
         "Fun fact: I have not, but I have been an Elder of my church for a long time now. When the pastor is out, I've stepped up. If we didn't have a pastor, I filled that role. (Not that anyone asked, but I'm Lutheran, and I do have schooling for the pastoral profession. If I take that profession farther, I'll be doing freelancing on the side.)",
       answer: negativeAnsw,
     },
-    { response: 'My lucky number is 17!', answer: 17 },
+    { response: 'My lucky number is 17!', answer: '17' },
+    { response: 'Here\'s all the possible answers', answer: ['appreciate', 'enjoy', 'adore', 'relish', 'desire', 'cherish', 'worship']
+    },
   ],
 }
 
@@ -62,26 +65,38 @@ for (let i = 0; i < quiz.questions.length; i++) {
     alert('Correct! ' + quiz.answers[i].response)
     //console.log(quiz.answers[i].response)
   } else if (i === 5) {
-    console.log('hit')
+    //console.log('hit')
     let numAttempts = 3
     let numCorrect = false
     console.log(quiz.answers[i].answer)
     while(numAttempts>0){
+      //console.log('hi')
       if(guestAnswer === quiz.answers[i].answer){
-        console.log('This stupid thing IS NEVER ACTUALLY HITTING AND THERE IS NO TA AND I DON"T KNOW WHY!!!!!! WHY WON\'t SOMETHING WORK RIGHT FOR ME ONCE IN THIS DAMN CLASS?!?!?!?')
-      }else if(guestAnswer < quiz.answers[i].answer) {
+        score++
+        //console.log('score added', score)
+        numCorrect = true
+        break;
+      }else if(+guestAnswer < quiz.answers[i].answer) {
         alert('That\'s a little low. Try again please')
+        console.log('current guestAnswer:', guestAnswer)
         guestAnswer = prompt(quiz.questions[i])
         
-        console.log('bang', numAttempts)
-      } else if (guestAnswer > quiz.answers[i].answer){
+        //console.log('bang', numAttempts)
+      } else if (+guestAnswer > quiz.answers[i].answer){
         alert('That\'s a little high. Try again please')
         guestAnswer = prompt(quiz.questions[i])
-        console.log('wham', numAttempts)
+       //console.log('wham', numAttempts)
         
       }
     numAttempts--
+    //console.log('running now.')
+
     }
+      if(numCorrect === false){
+        alert("Sorry, that's incorrect. " + quiz.answers[i].response)
+      }
+        alert('Correct. ' + quiz.answers[i].response)
+        
   } else {
     //console.log('incorrect')
     //console.log('Sorry, that\'s incorrect. ', quiz.answers[i].response)
